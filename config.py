d776dde737b20e9f0e7114d2f59ed6b39bc13321
Youhao Wei <charixandra@hotmail.com>
2017-08-03 01:46:31 -0700

    try to save heroki

--- config.py
+++ config.py
@@ -16,6 +16,7 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
+    MAIL_USE_SSL = False
     MAIL_USERNAME = 'internationalfriendstucson@gmail.com'
     MAIL_PASSWORD = 'WorldFriends717'
     MAIL_SENDER = 'internationalfriendstucson@gmail.com'
