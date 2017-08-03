11b5d7476cc5b199ac16566a23c32d8da7a37724
Youhao Wei <charixandra@hotmail.com>
2017-08-03 01:30:43 -0700

    update

--- config.py
+++ config.py
@@ -16,9 +16,8 @@ class Config:
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get(
-        'MAIL_USERNAME') or 'internationalfriendstucson@gmail.com'
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'WorldFriends717'
+    MAIL_USERNAME = 'internationalfriendstucson@gmail.com'
+    MAIL_PASSWORD = 'WorldFriends717'
     MAIL_SENDER = 'internationalfriendstucson@gmail.com'
     MAIL_SUBJECT_PREFIX = "[FROM International Friends]"
 
